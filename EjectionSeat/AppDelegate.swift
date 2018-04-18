4cd77064047e186b668a8abef8c7fc74cacd758f
Austin Kootz <akootz@nso.edu>
2018-04-17 22:47:12 -0600

    Changes to guard statements
    Signed-off-by: Austin Kootz <akootz@nso.edu>

--- EjectionSeat/AppDelegate.swift
+++ EjectionSeat/AppDelegate.swift
@@ -10,17 +10,19 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+    
     @IBOutlet weak var statusMenu: NSMenu!
     let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
-
+    
     @IBAction func ejectAll(_ sender: Any) {
-        if let urls = getURLList() {
-            for url in urls {
-                FileManager().unmountVolume(at: url, options: [.allPartitionsAndEjectDisk, .withoutUI], completionHandler: ejectionhandle)
-            }
+        guard let urls = getURLList(), urls.count > 0 else {
+            return
+        }
+        for url in urls {
+            FileManager().unmountVolume(at: url, options: [.allPartitionsAndEjectDisk, .withoutUI], completionHandler: ejectionhandle)
         }
     }
+    
     
     @IBAction func quit(_ sender: Any) {
         NSApplication.shared.terminate(self)
@@ -30,39 +32,36 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         if ($0 == nil) {
             //print("Ejected: ")
         }else{
-            //print("Failed to eject: ")
+            //print("Eject Failed: ")
         }
     }
     
     func getURLList()->[URL]?{
         let keys: [URLResourceKey] = [.volumeNameKey, .volumeIsRemovableKey, .volumeIsEjectableKey]
-        if var urls = FileManager().mountedVolumeURLs(includingResourceValuesForKeys: keys, options: []) {
-            for url in urls{
-                let components = url.pathComponents
-                if components.count <= 1 || components[1] != "Volumes"{
-                    urls.remove(at: urls.index(of: url)!)
-                }
-            }
-            return urls;
-        }
-        else{
+        guard var urls = FileManager().mountedVolumeURLs(includingResourceValuesForKeys: keys, options: []) else {
             return nil
         }
+        for url in urls{
+            let components = url.pathComponents
+            if components.count <= 1 || components[1] != "Volumes"{
+                urls.remove(at: urls.index(of: url)!)
+            }
+        }
+        return urls;
     }
     
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
         //statusItem.title = "EjectionSeat"
         statusItem.menu = statusMenu
         let icon = NSImage(named: NSImage.Name(rawValue: "USBIcon"))
         statusItem.image = icon
         statusItem.image?.isTemplate = true
     }
-
+    
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
     }
-
-
+    
+    
 }
 
