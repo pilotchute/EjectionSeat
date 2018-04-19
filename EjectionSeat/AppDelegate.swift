d1791e9eaab7034255acc954171634f54a003bb6
Austin Kootz <akootz@nso.edu>
2018-04-19 15:42:34 -0600

    Added about menu option and window.
    Signed-off-by: Austin Kootz <akootz@nso.edu>

--- EjectionSeat/AppDelegate.swift
+++ EjectionSeat/AppDelegate.swift
@@ -9,23 +9,26 @@
 import Cocoa
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate, NSUserNotificationCenterDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate, NSUserNotificationCenterDelegate, NSWindowDelegate {
     
     let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
     let menu: NSMenu = NSMenu()
+    var aboutWindow: NSWindow?
     var lastVolume: String = ""
+    let version: String? = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as! String
+
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         //statusItem.title = "EjectionSeat"
-        let icon = NSImage(named: NSImage.Name("USBIcon"))
-        statusItem.image = icon
+        statusItem.image = NSImage(named: NSImage.Name("USBIcon"))
         statusItem.image?.isTemplate = true
         statusItem.menu = menu
         statusItem.menu?.delegate = self
         makeMenu()
+        makeAboutWindow()
     }
     
-    func makeMenu(){
+    func makeMenu() {
         menu.removeAllItems()
         if let subMenu = makeSubMenu() {
             menu.addItem(NSMenuItem(title: "Eject All", action: #selector(AppDelegate.ejectAll(_:)), keyEquivalent: "e"))
@@ -35,6 +38,7 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate, NSUserNotifi
             menu.addItem(NSMenuItem(title: "Nothing to eject", action: nil, keyEquivalent: ""))
         }
         menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem(title: "About", action: #selector(AppDelegate.aboutWindowDisplay(_:)), keyEquivalent: "a"))
         menu.addItem(NSMenuItem(title: "Quit", action: #selector(AppDelegate.quit(_:)), keyEquivalent: "q"))
     }
     
@@ -42,7 +46,49 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate, NSUserNotifi
         makeMenu()
     }
     
-    @objc func makeSubMenu() -> NSMenu?{
+    func makeAboutWindow(){
+        let aboutView = NSView(frame: NSMakeRect(0, 0, 192, 256))
+        let aboutImage = NSImageView(image: NSImage(named: NSImage.Name("AppIcon"))!)
+        let aboutTextTitle = NSTextView(frame: NSMakeRect(0, 0, 192, 16))
+        let aboutTextBody = NSTextView(frame: NSMakeRect(0, 0, 192, 12))
+        
+        aboutView.addSubview(aboutImage)
+        aboutView.addSubview(aboutTextTitle)
+        aboutView.addSubview(aboutTextBody)
+        
+        aboutImage.setFrameSize(NSSize(width: 192, height: 128))
+        aboutImage.setFrameOrigin(NSPoint(x:0,y:96))
+        aboutImage.imageAlignment = NSImageAlignment.alignCenter
+        
+        aboutTextTitle.setFrameOrigin(NSPoint(x:0,y:64))
+        aboutTextTitle.font = NSFont.titleBarFont(ofSize: 16)
+        aboutTextTitle.textColor = NSColor.textColor
+        aboutTextTitle.alignment = NSTextAlignment.center
+        aboutTextTitle.backgroundColor = NSColor.clear
+        aboutTextTitle.insertText("EjectionSeat.app")
+        aboutTextTitle.isEditable = false
+        
+        aboutTextBody.setFrameOrigin(NSPoint(x:0,y:40))
+        aboutTextBody.font = NSFont.systemFont(ofSize: 12)
+        aboutTextBody.textColor = NSColor.gray
+        aboutTextBody.alignment = NSTextAlignment.center
+        aboutTextBody.backgroundColor = NSColor.clear
+        aboutTextBody.insertText("Developed by Åustin Kootz\nVersion \(version!)")
+        aboutTextBody.isEditable = false
+        
+        aboutWindow = NSWindow.init(contentRect: aboutView.frame, styleMask: [.titled, .closable], backing: NSWindow.BackingStoreType.buffered, defer:false)
+        aboutWindow?.contentView = aboutView
+        aboutWindow?.backgroundColor = NSColor.windowBackgroundColor
+        aboutWindow?.isReleasedWhenClosed = false
+        aboutWindow?.title = "About EjectionSeat"
+    }
+    
+    @objc func aboutWindowDisplay(_ sender:NSMenuItem) {
+        aboutWindow?.cascadeTopLeft(from: NSEvent.mouseLocation)
+        aboutWindow?.orderFrontRegardless()
+    }
+    
+    @objc func makeSubMenu() -> NSMenu? {
         guard let urls = getURLList(), urls.count > 0 else { return nil }
         let subMenu = NSMenu()
         var titles: [String] = []
@@ -62,13 +108,13 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate, NSUserNotifi
         NSApplication.shared.terminate(self)
     }
     
-    @objc func eject(_ sender: NSMenuItem){
+    @objc func eject(_ sender: NSMenuItem) {
         guard let urls = getURLList(), urls.count > 0 else { return }
         for url in urls {
             if url.pathComponents[url.pathComponents.endIndex-1] == sender.title {
                 lastVolume = sender.title
                 guard (try? NSWorkspace().unmountAndEjectDevice(at: url)) != nil else {
-                        showNotificationFailure()
+                    showNotificationFailure()
                     return
                 }
                 showNotificationSuccess()
@@ -123,7 +169,7 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate, NSUserNotifi
         NSUserNotificationCenter.default.deliver(notification)
     }
     
-    func userNotificationCenter(_ center: NSUserNotificationCenter, didActivate notification: NSUserNotification){
+    func userNotificationCenter(_ center: NSUserNotificationCenter, didActivate notification: NSUserNotification) {
         eject(NSMenuItem(title: notification.title!, action: nil, keyEquivalent: ""))
     }
     
@@ -131,12 +177,12 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate, NSUserNotifi
         return true
     }
     
-    func getURLList()->[URL]?{
+    func getURLList()->[URL]? {
         let keys: [URLResourceKey] = [.volumeNameKey, .volumeIsRemovableKey, .volumeIsEjectableKey]
         guard var urls = FileManager().mountedVolumeURLs(includingResourceValuesForKeys: keys, options: []) else {
             return nil
         }
-        for url in urls{
+        for url in urls {
             let components = url.pathComponents
             if components.count < 2 || components[1] != "Volumes"{
                 urls.remove(at: urls.index(of: url)!)
