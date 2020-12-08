829df435c9780597af1fc6429341bbcbd39540df
pilotchute <alea@kootz.com>
2020-12-08 16:12:48 -0700

    Update for Big Sur
    Signed-off-by: pilotchute <alea@kootz.com>

--- ejectionseat.rb
+++ ejectionseat.rb
@@ -1,8 +1,8 @@
 cask 'ejectionseat' do
-  version '1.4.4'
-  sha256 'c2b6920ffa1f477a5542994045b8c583cffa2373d56d50c67e73745830cd6d17'
+  version '1.5.0'
+  sha256 "d323e3491fe71807e746b5b7629d1bb41d05fa560e2a55c75e4476213630ecc9"
 
-  url "https://github.com/pilotchute/EjectionSeat/releases/download/#{version}/EjectionSeat.app.zip"
+  url "https://github.com/pilotchute/EjectionSeat/releases/download/#{version}/EjectionSeat.zip"
   appcast 'https://github.com/pilotchute/EjectionSeat/releases.atom'
   name 'EjectionSeat'
   homepage 'https://github.com/pilotchute/EjectionSeat'
